3952588f14c34bb972b352860c6b2a1dcc981d9d
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-01-27 12:59:24 +0200

    Add class for all http request

--- public/js/js_auth.js
+++ public/js/js_auth.js
@@ -25,3 +25,28 @@ function includeHTML() {
     }
   }
 };
+function login(){
+  const params = {
+        // email: document.querySelector('#email').value,
+        // password: document.querySelector('#password').value
+        email: 'Akimana',
+        password: 'Umuhuza'
+    }
+    var request = new HttpRequest();
+    request.method = "POST";
+    request.data = JSON.stringify(params);
+    request.url = "http://localhost:5600/user.signin";
+
+    //create callback for success containing the response
+    request.success = function(response) {
+        console.log('response: '+response);
+    };
+
+    //and a fail callback containing the error
+    request.fail = function(error) {
+        console.log('Error:'+error);
+    };
+    // console.log('params:'+JSON.stringify(params))
+    //and finally send it away
+    request.send();
+}
