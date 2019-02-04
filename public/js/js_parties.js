3046c5cf50b2f2a49a4d5d096fe9fd644ba71a25
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-02-04 19:55:25 +0200

    [finishes #163705243] Correction of bug error in editing pary

--- public/js/js_parties.js
+++ public/js/js_parties.js
@@ -28,7 +28,7 @@ let app = new function() {
 
 	this.fetchParties = (url, displayDiv)=>{
 		let request = new HttpRequest();
-		request.method = "GET";
+		request.method = 'GET';
 		request.url = url;
 		request.success = (res) => {
 			let response = JSON.parse(res);
@@ -38,15 +38,15 @@ let app = new function() {
 			}
 		};
 		request.fail = (error) => {
-			var error = JSON.parse(error)
+			var error = JSON.parse(error);
 			Notifier('',error.error);
 			displayDiv.innerHTML = '<h3 class="text-center">Nothing to display</h3>';
 		};
 		request.send();
-	}
+	};
 	this.fetchOffices = (url, displayDiv)=>{
 		let request = new HttpRequest();
-		request.method = "GET";
+		request.method = 'GET';
 		request.url = url;
 		request.success = (res) => {
 			let response = JSON.parse(res);
@@ -56,27 +56,27 @@ let app = new function() {
 			}
 		};
 		request.fail = (error) => {
-			var error = JSON.parse(error)
+			var error = JSON.parse(error);
 			Notifier('',error.error);
 			displayDiv.innerHTML = '<h3 class="text-center">Nothing to display</h3>';
 		};
 		request.send();
-	}
+	};
 	this.fetchOne = (url)=>{
 		this.itemInfo = {};
 		let request = new HttpRequest();
-		request.method = "GET";
+		request.method = 'GET';
 		request.url = url;
 		request.success = (res) => {
 			let response = JSON.parse(res);
 			this.itemInfo = response.data[0];
 		};
 		request.fail = (error) => {
-			var error = JSON.parse(error)
+			var error = JSON.parse(error);
 			Notifier('',error.error);
 		};
 		request.send();
-	}
+	};
 
 	this.saveNewPary = () => {
 		if(!this.partyName.value){
@@ -96,8 +96,8 @@ let app = new function() {
 		}
 		this.params = {name: this.partyName.value, hqAddress: this.partyAddress.value, logoUrl: this.partyLogo.value};
 		this.params = JSON.stringify(this.params);
-		this.addItem('/v1/parties', this.params, this.partiesEl, this.partyDesign)
-	}
+		this.addItem('/v1/parties', this.params, this.partiesEl, this.partyDesign);
+	};
 	this.editParty = (partyId, partyName) => {
 		if(!this.partyName.value){
 			Notifier('','Please enter party name');
@@ -116,8 +116,8 @@ let app = new function() {
 		}
 		this.params = {name: this.partyName.value, hqAddress: this.partyAddress.value, logoUrl: this.partyLogo.value};
 		this.params = JSON.stringify(this.params);
-		this.editItem('/v1/parties/'+partyId+'/'+partyName, this.params, this.partiesEl, this.partyDesign)
-	}
+		this.editItem('/v1/parties/'+partyId+'/'+partyName, this.params, this.partiesEl, this.partyDesign);
+	};
 	this.saveNewOffice = () => {
 		if(!this.officeName.value){
 			Notifier('','Please enter party name');
@@ -131,8 +131,8 @@ let app = new function() {
 		}
 		this.params = {name: this.officeName.value, type: this.officeType.value};
 		this.params = JSON.stringify(this.params);
-		this.addItem('/v1/offices', this.params, this.officesEl, this.officeDesign)
-	}
+		this.addItem('/v1/offices', this.params, this.officesEl, this.officeDesign);
+	};
 	this.editOffice = (officeId, officeName) => {
 		if(!this.officeName.value){
 			Notifier('','Please enter party name');
@@ -146,12 +146,12 @@ let app = new function() {
 		}
 		this.params = {name: this.officeName.value, type: this.officeType.value};
 		this.params = JSON.stringify(this.params);
-		this.editItem('/v1/offices/'+officeId+'/'+officeName, this.params, this.officesEl, this.officeDesign)
-	}
+		this.editItem('/v1/offices/'+officeId+'/'+officeName, this.params, this.officesEl, this.officeDesign);
+	};
 	// Html design
 	this.partyDesign = (designData, hideAction)=>{
 		let panelDivCss = hideAction?'hide':'show';
-		let party = ''
+		let party = '';
 		party += '<div class="col-smt-12 col-tab-4 col-lap-3 col-dsk-3" id="id_'+ designData.id +'">';
 		party += '<div class="panel">';
 		party += '<div class="panel-heading">'+ designData.name +'</div>';
@@ -167,7 +167,7 @@ let app = new function() {
 		party += '</div>';
 		party += '</div>';
 		return party;
-	}
+	};
 
 	this.officeDesign = (designData, hideAction) => {
 		let panelDivCss = hideAction?'hide':'show';
@@ -187,7 +187,7 @@ let app = new function() {
 		office += '</div>';
 		office += '</div>';
 		return office;
-	}
+	};
 	this.openModal = (modalToOpen,action,modalData) => {
 		var modal = document.getElementById(modalToOpen);
 		let officeModTitle = document.getElementById('office-modal-title');
@@ -195,72 +195,72 @@ let app = new function() {
 		modal.style.display = 'block';
 		if(action=='addParty'){
 			partyModTitle.textContent='Add new party';
-			this.editPartyBtn.style.visibility = 'hidden'
-			this.partyFormBtn.style.visibility = 'visible'
+			this.editPartyBtn.style.visibility = 'hidden';
+			this.partyFormBtn.style.visibility = 'visible';
 			this.partyFormBtn.onclick = () => {
 				this.saveNewPary();
-			}
+			};
 		}
 		if(action=='addOffice'){
 			officeModTitle.textContent='Add new office';
-			this.editOfficeBtn.style.visibility = 'hidden'
-			this.officeFormBtn.style.visibility = 'visible'
+			this.editOfficeBtn.style.visibility = 'hidden';
+			this.officeFormBtn.style.visibility = 'visible';
 			this.officeFormBtn.onclick = () => {
-				this.saveNewOffice()
-			}
+				this.saveNewOffice();
+			};
 		}
 		if(action=='editParty'){
 			var party = this.itemDetail(this.partiesList, modalData);
 			this.currentParty = party;
 			partyModTitle.innerHTML='Edit party <strong>'+party.name+'</strong>';
-			this.editPartyBtn.style.visibility = 'visible'
-			this.partyFormBtn.style.visibility = 'hidden'
+			this.editPartyBtn.style.visibility = 'visible';
+			this.partyFormBtn.style.visibility = 'hidden';
 			this.partyName.value = party.name;
 			this.partyAddress.value = party.hqAddress;
 			this.partyLogo.value = party.logoUrl;
 			this.editPartyBtn.textContent = 'Edit '+party.name;
 			this.editPartyBtn.onclick = () => {
-				this.editParty(party.id,party.name)
-			}
+				this.editParty(party.id,party.name);
+			};
 		}
 		if(action=='editOffice'){
 			var office = this.itemDetail(this.officesList, modalData);
 			this.currentOffice = office;
 			officeModTitle.innerHTML='Delete office <strong>'+office.name+'</strong>';
-			this.editOfficeBtn.style.visibility = 'visible'
-			this.officeFormBtn.style.visibility = 'hidden'
+			this.editOfficeBtn.style.visibility = 'visible';
+			this.officeFormBtn.style.visibility = 'hidden';
 			this.officeName.value = office.name;
 			this.officeType.value = office.type;
 			this.editOfficeBtn.textContent = 'Edit '+office.name;
 			this.officeFormBtn.onclick = () => {
-				this.editOffice(office.id,office.name)
-			}
+				this.editOffice(office.id,office.name);
+			};
 		}
 		if(action=='deleteParty'){
 			var party = this.itemDetail(this.partiesList, modalData);
 			this.currentParty = party;
 			this.delItemModalTitle.innerHTML='Delete party <strong>'+party.name+'</strong>';
 			this.deleteBtn.onclick = () => {
-				this.deleteItem('/v1/parties/'+party.id, party.id,'parties-list')
-			}
+				this.deleteItem('/v1/parties/'+party.id, party.id,'parties-list');
+			};
 		}
 		if(action=='deleteOffice'){
 			var office = this.itemDetail(this.officesList, modalData);
 			this.currentOffice = office;
 			this.delItemModalTitle.innerHTML='Edit office <strong>'+office.name+'</strong>';
 			this.deleteBtn.onclick = () => {
-				this.deleteItem('/v1/offices/'+office.id, office.id,'offices-list')
-			}
+				this.deleteItem('/v1/offices/'+office.id, office.id,'offices-list');
+			};
 		}
-	}
+	};
 	// When the user clicks on <span> (x), close the modal
 	this.closeModal = (modalToClose) => {
 		var modal = document.getElementById(modalToClose);
 		modal.style.display = 'none';
-	}
+	};
 	this.addItem = (url, dataToSend, displayEl, designEl) => {
 		let request = new HttpRequest();
-		request.method = "POST";url
+		request.method = 'POST';url;
 		request.data = dataToSend;
 		request.url = url;
 		request.success = function(res) {
@@ -276,31 +276,31 @@ let app = new function() {
 			this.params = {};
 		};
 		request.fail = function(error) {
-			var error = JSON.parse(error)
-			console.log('Error:'+JSON.stringify(error))
+			var error = JSON.parse(error);
+			console.log('Error:'+JSON.stringify(error));
 			Notifier('',error.error);
 		};
 		request.send();
-	}
+	};
 	this.deleteItem = (url, id ,contentEl) => {
 		let request = new HttpRequest();
-		request.method = "DELETE";
+		request.method = 'DELETE';
 		request.url = url;
 		request.success = (res) => {
 			let response = JSON.parse(res);
 			Notifier('',response.message);
-			this.removeElement(contentEl, 'id_'+id)
+			this.removeElement(contentEl, 'id_'+id);
 			closeModal('deleteModal');
 		};
 		request.fail = (error) => {
-			var error = JSON.parse(error)
+			var error = JSON.parse(error);
 			Notifier('',error.error);
 		};
 		request.send();
-	}
+	};
 	this.editItem = (url, dataToSend, displayEl, designEl) => {
 		let request = new HttpRequest();
-		request.method = "PATCH";
+		request.method = 'PATCH';
 		request.data = dataToSend;
 		request.url = url;
 		request.success = function(res) {
@@ -316,12 +316,12 @@ let app = new function() {
 			this.params = {};
 		};
 		request.fail = function(error) {
-			var error = JSON.parse(error)
-			console.log('Error:'+JSON.stringify(error))
+			var error = JSON.parse(error);
+			console.log('Error:'+JSON.stringify(error));
 			Notifier('',error.error);
 		};
 		request.send();
-	}
+	};
 	this.itemDetail = (items, itemId) => {
 		let detail = {};
 		for(let i = 0; i <= items.length; i++){
@@ -331,7 +331,7 @@ let app = new function() {
 			}
 		}
 		return detail;
-	}
+	};
 	this.removeElement = (parentDiv, childDiv) => {
 		if (childDiv == parentDiv) {
 			Notifier('','Item cannot be removed.');
@@ -345,7 +345,7 @@ let app = new function() {
 			Notifier('Item has already been removed or does not exist.');
 			return false;
 		}
-	}
-}
+	};
+};
 app.fetchOffices('/v1/offices', app.officesEl);
 app.fetchParties('/v1/parties', app.partiesEl);
