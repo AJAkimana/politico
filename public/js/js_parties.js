3e5f5b2a7cdb10c445d2b318b42013970c6c15d8
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-02-05 19:14:18 +0200

    Correction of files structure

--- public/js/js_parties.js
+++ public/js/js_parties.js
@@ -24,17 +24,20 @@ let app = new function() {
 	this.editOfficeBtn = document.getElementById('edit-office');
 	this.params = {};
 
+	this.officesUrl = '/v1/offices';
+	this.partiesUrl = '/v1/parties';
+
 	this.hideAction = false;
 
-	this.fetchParties = (url, displayDiv)=>{
+	this.fetchParties = ()=>{
 		let request = new HttpRequest();
 		request.method = 'GET';
-		request.url = url;
+		request.url = this.partiesUrl;
 		request.success = (res) => {
 			let response = JSON.parse(res);
 			this.partiesList = response.data;
 			for(let i = 0; i < this.partiesList.length; i++){
-				displayDiv.innerHTML += this.partyDesign(this.partiesList[i],this.hideAction);
+				this.partiesEl.innerHTML += this.partyDesign(this.partiesList[i],this.hideAction);
 			}
 		};
 		request.fail = (error) => {
@@ -44,15 +47,15 @@ let app = new function() {
 		};
 		request.send();
 	};
-	this.fetchOffices = (url, displayDiv)=>{
+	this.fetchOffices = () =>{
 		let request = new HttpRequest();
 		request.method = 'GET';
-		request.url = url;
+		request.url = this.officesUrl;
 		request.success = (res) => {
 			let response = JSON.parse(res);
 			this.officesList = response.data;
 			for(let i = 0; i < this.officesList.length; i++){
-				displayDiv.innerHTML += this.officeDesign(this.officesList[i],this.hideAction);
+				this.officesEl.innerHTML += this.officeDesign(this.officesList[i],this.hideAction);
 			}
 		};
 		request.fail = (error) => {
@@ -63,7 +66,6 @@ let app = new function() {
 		request.send();
 	};
 	this.fetchOne = (url)=>{
-		this.itemInfo = {};
 		let request = new HttpRequest();
 		request.method = 'GET';
 		request.url = url;
@@ -96,7 +98,7 @@ let app = new function() {
 		}
 		this.params = {name: this.partyName.value, hqAddress: this.partyAddress.value, logoUrl: this.partyLogo.value};
 		this.params = JSON.stringify(this.params);
-		this.addItem('/v1/parties', this.params, this.partiesEl, this.partyDesign);
+		this.addItem(this.partiesUrl, this.params, this.partiesEl, this.partyDesign);
 	};
 	this.editParty = (partyId, partyName) => {
 		if(!this.partyName.value){
@@ -116,7 +118,7 @@ let app = new function() {
 		}
 		this.params = {name: this.partyName.value, hqAddress: this.partyAddress.value, logoUrl: this.partyLogo.value};
 		this.params = JSON.stringify(this.params);
-		this.editItem('/v1/parties/'+partyId+'/'+partyName, this.params, this.partiesEl, this.partyDesign);
+		this.editItem(+this.partiesUrl+'/'+partyId+'/'+partyName, this.params, this.partiesEl, this.partyDesign);
 	};
 	this.saveNewOffice = () => {
 		if(!this.officeName.value){
@@ -131,7 +133,7 @@ let app = new function() {
 		}
 		this.params = {name: this.officeName.value, type: this.officeType.value};
 		this.params = JSON.stringify(this.params);
-		this.addItem('/v1/offices', this.params, this.officesEl, this.officeDesign);
+		this.addItem(this.officesUrl, this.params, this.officesEl, this.officeDesign);
 	};
 	this.editOffice = (officeId, officeName) => {
 		if(!this.officeName.value){
@@ -146,8 +148,17 @@ let app = new function() {
 		}
 		this.params = {name: this.officeName.value, type: this.officeType.value};
 		this.params = JSON.stringify(this.params);
-		this.editItem('/v1/offices/'+officeId+'/'+officeName, this.params, this.officesEl, this.officeDesign);
+		this.editItem(this.officesUrl+'/'+officeId+'/'+officeName, this.params, this.officesEl, this.officeDesign);
 	};
+	this.setPartyFormData = (party) => {
+		this.partyName.value = party.name;
+		this.partyAddress.value = party.hqAddress;
+		this.partyLogo.value = party.logoUrl;
+	}
+	this.setOfficeFormData = (office) => {
+		this.officeName.value = office.name;
+		this.officeType.value = office.type;
+	}
 	// Html design
 	this.partyDesign = (designData, hideAction)=>{
 		let panelDivCss = hideAction?'hide':'show';
@@ -197,6 +208,7 @@ let app = new function() {
 			partyModTitle.textContent='Add new party';
 			this.editPartyBtn.style.visibility = 'hidden';
 			this.partyFormBtn.style.visibility = 'visible';
+			this.setPartyFormData({name:'',hqAddress:'',logoUrl:''});
 			this.partyFormBtn.onclick = () => {
 				this.saveNewPary();
 			};
@@ -205,6 +217,7 @@ let app = new function() {
 			officeModTitle.textContent='Add new office';
 			this.editOfficeBtn.style.visibility = 'hidden';
 			this.officeFormBtn.style.visibility = 'visible';
+			this.setOfficeFormData({name:'',type:''});
 			this.officeFormBtn.onclick = () => {
 				this.saveNewOffice();
 			};
@@ -215,9 +228,9 @@ let app = new function() {
 			partyModTitle.innerHTML='Edit party <strong>'+party.name+'</strong>';
 			this.editPartyBtn.style.visibility = 'visible';
 			this.partyFormBtn.style.visibility = 'hidden';
-			this.partyName.value = party.name;
-			this.partyAddress.value = party.hqAddress;
-			this.partyLogo.value = party.logoUrl;
+			
+			this.setPartyFormData(party);
+
 			this.editPartyBtn.textContent = 'Edit '+party.name;
 			this.editPartyBtn.onclick = () => {
 				this.editParty(party.id,party.name);
@@ -227,6 +240,7 @@ let app = new function() {
 			var office = this.itemDetail(this.officesList, modalData);
 			this.currentOffice = office;
 			officeModTitle.innerHTML='Delete office <strong>'+office.name+'</strong>';
+			this.setOfficeFormData(office);
 			this.editOfficeBtn.style.visibility = 'visible';
 			this.officeFormBtn.style.visibility = 'hidden';
 			this.officeName.value = office.name;
@@ -241,7 +255,7 @@ let app = new function() {
 			this.currentParty = party;
 			this.delItemModalTitle.innerHTML='Delete party <strong>'+party.name+'</strong>';
 			this.deleteBtn.onclick = () => {
-				this.deleteItem('/v1/parties/'+party.id, party.id,'parties-list');
+				this.deleteItem(this.partiesUrl+'/'+party.id, party.id,'parties-list');
 			};
 		}
 		if(action=='deleteOffice'){
@@ -249,7 +263,7 @@ let app = new function() {
 			this.currentOffice = office;
 			this.delItemModalTitle.innerHTML='Edit office <strong>'+office.name+'</strong>';
 			this.deleteBtn.onclick = () => {
-				this.deleteItem('/v1/offices/'+office.id, office.id,'offices-list');
+				this.deleteItem(this.officesUrl+'/'+office.id, office.id,'offices-list');
 			};
 		}
 	};
@@ -266,14 +280,8 @@ let app = new function() {
 		request.success = function(res) {
 			let response = JSON.parse(res);
 			displayEl.innerHTML += designEl(response.data[0],this.hideAction);
-			if(this.partyName) this.partyName.value = '';
-			if(this.partyAddress) this.partyAddress.value = '';
-			if(this.partyLogo) this.partyLogo.value = '';
-			if(this.officeName) this.officeName.value = '';
-			if(this.officeType) this.officeType.value = '';
 			closeModal('partyModal');
 			closeModal('officeModal');
-			this.params = {};
 		};
 		request.fail = function(error) {
 			var error = JSON.parse(error);
@@ -305,15 +313,14 @@ let app = new function() {
 		request.url = url;
 		request.success = function(res) {
 			let response = JSON.parse(res);
-			// displayEl.innerHTML += designEl(response.data[0],this.hideAction);
-			if(this.partyName) this.partyName.value = '';
-			if(this.partyAddress) this.partyAddress.value = '';
-			if(this.partyLogo) this.partyLogo.value = '';
-			if(this.officeName) this.officeName.value = '';
-			if(this.officeType) this.officeType.value = '';
+
+			if(displayEl == this.officesEl){
+				this.fetchOffices();
+			}else{
+				this.fetchParties()
+			}
 			closeModal('partyModal');
 			closeModal('officeModal');
-			this.params = {};
 		};
 		request.fail = function(error) {
 			var error = JSON.parse(error);
@@ -333,6 +340,7 @@ let app = new function() {
 		return detail;
 	};
 	this.removeElement = (parentDiv, childDiv) => {
+		console.log('P:'+parentDiv, 'C:'+childDiv)
 		if (childDiv == parentDiv) {
 			Notifier('','Item cannot be removed.');
 		}
@@ -347,5 +355,5 @@ let app = new function() {
 		}
 	};
 };
-app.fetchOffices('/v1/offices', app.officesEl);
-app.fetchParties('/v1/parties', app.partiesEl);
+app.fetchOffices();
+app.fetchParties();
