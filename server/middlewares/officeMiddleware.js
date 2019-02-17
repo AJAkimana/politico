b2339bb5a35f2bad7b7fde31d931a8d05fc81653
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-02-17 06:24:37 +0200

    Introduce new validatoror

--- server/middlewares/officeMiddleware.js
+++ server/middlewares/officeMiddleware.js
@@ -2,21 +2,17 @@ const officeTypes = ['federal','legistrative','state','local'];
 
 const officeMiddleware = {
 	verifyOfficeBody(req, res, next){
-		req.assert('name', 'Type party name').notEmpty();
-		req.assert('type', 'Invalid office type').isIn(officeTypes);
+		req.assert('name', 'Type office name').notEmpty();
+		req.assert('name', 'Invalid office name').isString();
+		req.assert('type', 'Specifiy office type').notEmpty();
 
 		const errors = req.validationErrors();
+		const numberOfKeys = Object.keys(req.body).length;
+		const type = req.body.type.toLowerCase().trim();
+		console.log('Office:'+type+', Index :'+officeTypes.indexOf(type))
 		if (errors) return res.status(400).json({status: 400, error: errors[0].msg});
-
-		return next();
-	},
-	verifyOfficeBodyWithId(req, res, next){
-		req.assert('name', 'Type party name').notEmpty();
-		req.assert('type', 'Invalid office type').isIn(officeTypes);
-		req.assert('officeId','Invalid office spacification').isInt();
-		
-		const errors = req.validationErrors();
-		if (errors) return res.status(400).json({status: 400, error: errors[0].msg});
+		if (numberOfKeys !== 2) return res.status(400).json({status: 400, error: 'Invalid information'});
+		if (officeTypes.indexOf(type) ===-1 ) return res.status(400).json({status: 400, error: 'Invalid office type'});
 
 		return next();
 	},
--- server/middlewares/partyMiddleware.js
+++ server/middlewares/partyMiddleware.js
@@ -1,22 +1,14 @@
 const partyMiddleware = {
 	verifyPartyBody(req, res, next){
-		req.assert('name', 'Type party name').notEmpty();
-		req.assert('hqAddress', 'Provide party address').notEmpty();
-		req.assert('logoUrl', 'Provide party logo url').notEmpty();
+		req.assert('name', 'Type office name').notEmpty();
+		req.assert('name', 'Invalid office name').notOnlyWhiteSpace().isString();
+		req.assert('hqAddress', 'Provide party address').notEmpty().notOnlyWhiteSpace().isString();
+		req.assert('logoUrl', 'Provide party logo url').notEmpty().notOnlyWhiteSpace().isString();
 
 		const errors = req.validationErrors();
+		const numberOfKeys = Object.keys(req.body).length;
 		if (errors) return res.status(400).json({status: 400, error: errors[0].msg});
-
-		return next();
-	},
-	verifyPartyBodyWithId(req, res, next){
-		req.assert('name', 'Type party name').notEmpty();
-		req.assert('hqAddress', 'Provide party address').notEmpty();
-		req.assert('logoUrl', 'Provide party logo url').notEmpty();
-		req.assert('partyId','Invalid party spacification').isInt();
-		
-		const errors = req.validationErrors();
-		if (errors) return res.status(400).json({status: 400, error: errors[0].msg});
+		if (numberOfKeys !== 3) return res.status(400).json({status: 400, error: 'Invalid information'});
 
 		return next();
 	},
