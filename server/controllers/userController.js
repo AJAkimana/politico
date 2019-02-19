0bbe922d0b53dc13b7fb94321547eacfb5eea5ee
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-02-19 15:46:45 +0200

    Add method for Log in

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,7 +8,7 @@ import helper from '../helper/helper';
 */
 const insertQuery = 'INSERT INTO users(firstname, lastname, othername, email, password, phoneNumber, passportUrl) VALUES ($1, $2, $3, $4, $5, $6,$7) returning *';
 const queryAll = 'SELECT * FROM users';
-const queryOne = 'SELECT * FROM users WHERE id = $1';
+const queryOne = 'SELECT * FROM users WHERE email = $1';
 const initialise = () => {
 	UserDB.createUserTable();
 }
@@ -26,13 +26,43 @@ const userController = {
 			req.body.passportUrl,
 		] 
 		Runner.execute(insertQuery, values, (err, data)=>{
-			console.log(err, data)
 			if(err){
 				return res.status(500).json({ 
 					status: 500,
 					error: 'Service not availavle'
 				});
 			} 
+			const token = helper.generateToken(data.rows[0].id);
+			const response = [{token:token,user: data.rows[0]}]
+			res.status(201).json({
+				status: 201,
+				message: 'Successfully created',
+				data: response
+			});
+		})
+	},
+	userLogin(req, res){
+		initialise();
+		Runner.execute(queryOne, [req.body.email], (err, data)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			}
+			if(!data.rows[0]){
+				return res.status(401).json({ 
+					status: 401,
+					error: 'Email not found'
+				});
+			}
+			// console.log(data.rows[0].password, req.body.password)
+			if(!helper.comparePassword(data.rows[0].password, req.body.password)){
+				return res.status(401).json({ 
+					status: 401,
+					error: 'Invalid credentials'
+				});
+			}
 			const token = helper.generateToken(data.rows[0].id);
 			const response = [{token:token,user: data.rows[0]}]
 			res.status(201).json({
