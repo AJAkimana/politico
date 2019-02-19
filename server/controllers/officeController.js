35c7f5c783ba73965ba170a4a6b3ae05501d99c0
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-02-19 08:15:06 +0200

    [#164068250] Migrate from data structure to postgres db

--- server/controllers/officeController.js
+++ server/controllers/officeController.js
@@ -1,91 +1,141 @@
 import DataModel from '../models/DataModel';
-const officeFileJson = '../helper/data/offices.json';
-const offices = require(officeFileJson);
+
+const queryInsert = 'INSERT INTO offices(name, type) VALUES ($1, $2) returning *';
+const queryEdit = 'UPDATE offices SET name=$1, type=$2, updated_at=NOW() WHERE id=$3 returning *';
+const queryAll = 'SELECT * FROM offices';
+const queryOne = 'SELECT * FROM offices WHERE id = $1';
+const queryDelete = 'DELETE FROM offices WHERE id = $1';
 
 const officeController = {
 	createNewOffice(req, res){
-		req.body.type = req.body.type.toLowerCase().trim();
-		DataModel.saveNew(officeFileJson, offices, req.body)
-			.then(office => {
-				res.status(201).json({
-					status: 201,
-					message: 'Successfully created',
-					data: office
+		const values = [
+			req.body.name,
+			req.body.type.toLowerCase().trim(),
+		] 
+		DataModel.execute(queryInsert, values, (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
 				});
-			})
-			.catch(err => res.status(500).json({ 
-				status: 500,
-				error: err.message 
-			}));
+			} 
+			res.status(201).json({
+				status: 201,
+				message: 'Successfully created',
+				data: result.rows[0]
+			});
+		})
 	},
 	getAllOfficesList(req, res){
-		DataModel.findAll(offices, 'office')
-			.then(offices => res.status(200).json({
+		DataModel.execute(queryAll, [], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (result.rowCount < 1){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No office found'
+				});
+			}
+			res.status(200).json({
 				status: 200,
 				message: 'Success',
-				data: offices
-			}))
-			.catch(err => {
-				const codeStatus = err.status?err.status:500;
-				const response = {
-					status:codeStatus,
-					error:err.message
-				};
-				return res.status(codeStatus).json(response);
+				data: result.rows
 			});
+		})
 	},
 	getSpecificOffice(req, res){
 		const officeId = Number(req.params.officeId);
 
-		DataModel.findOneById(offices, 'office', officeId)
-			.then(office => res.status(200).json({
+		DataModel.execute(queryOne, [officeId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No office found'
+				});
+			}
+			res.status(200).json({
 				status: 200,
 				message: 'Success',
-				data: office
-			}))
-			.catch(err => {
-				const codeStatus = err.status?err.status:500;
-				const response = {
-					status:codeStatus,
-					error:err.message
-				};
-				return res.status(codeStatus).json(response);
+				data: result.rows[0]
 			});
+		})
 	},
 	modifyOffice(req, res){
 		req.body.type = req.body.type.toLowerCase().trim();
-		req.body.dataId = req.params.officeId;
-		DataModel.findOneAndUpdate(officeFileJson, offices, 'office', req.body)
-			.then(office => res.status(200).json({
-				status: 200,
-				message: 'Successfully modified',
-				data: office
-			}))
-			.catch(err => {
-				const codeStatus = err.status?err.status:500;
-				const response = {
-					status:codeStatus,
-					error:err.message
-				};
-				res.status(codeStatus).json(response);
-			});
+		const values = [
+			req.body.name,
+			req.body.type.toLowerCase().trim(),
+			req.params.officeId
+		] 
+		DataModel.execute(queryOne, [req.params.officeId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No office found'
+				});
+			}
+			DataModel.execute(queryEdit, values, (error, response)=>{
+				console.log(response)
+				if(error){
+					return res.status(500).json({ 
+						status: 500,
+						error: 'Service not availavle'
+					});
+				}
+				res.status(200).json({
+					status: 200,
+					message: 'Successfully modified',
+					data: response.rows[0]
+				});
+			})
+		})
 	},
 	deleteOffice(req, res){
 		const officeId = Number(req.params.officeId);
 
-		DataModel.removeOne(officeFileJson, offices, 'office', officeId)
-			.then(() => res.status(200).json({
-				status: 200,
-				message: 'The office has been deleted'
-			}))
-			.catch(err => {
-				const codeStatus = err.status?err.status:500;
-				const response = {
-					status:codeStatus,
-					error:err.message
-				};
-				res.status(codeStatus).json(response);
-			});
+		DataModel.execute(queryOne, [officeId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No office found'
+				});
+			}
+			DataModel.execute(queryDelete, [officeId], (error, response)=>{
+				console.log(response)
+				if(error){
+					return res.status(500).json({ 
+						status: 500,
+						error: 'Service not availavle'
+					});
+				}
+				res.status(200).json({
+					status: 200,
+					message: 'The office has been deleted'
+				});
+			})
+		})
 	}
 };
 
--- server/controllers/partyController.js
+++ server/controllers/partyController.js
@@ -1,94 +1,142 @@
 import DataModel from '../models/DataModel';
-const partyFileJson = '../helper/data/parties.json';
-const parties = require(partyFileJson);
+
+const queryInsert = 'INSERT INTO parties(name, hqAddress, logoUrl) VALUES ($1, $2, $3) returning *';
+const queryEdit = 'UPDATE parties SET name=$1, hqAddress=$2, logoUrl=$3, updated_at=NOW() WHERE id=$4';
+const queryAll = 'SELECT * FROM parties';
+const queryOne = 'SELECT * FROM parties WHERE id = $1';
+const queryDelete = 'DELETE FROM parties WHERE id = $1';
 
 const partyController = {
 	createNewParty(req, res){
-		DataModel.saveNew(partyFileJson, parties, req.body)
-			.then(party => {
-				res.status(201).json({
-					status: 201,
-					message: 'Successfully created',
-					data: party
-				});
-			})
-			.catch(err => {
-				console.log('Err:'+err);
-				res.status(500).json({ 
+		const values = [
+			req.body.name,
+			req.body.hqAddress,
+			req.body.logoUrl,
+		] 
+		DataModel.execute(queryInsert, values, (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
 					status: 500,
-					error: err.message 
+					error: 'Service not availavle'
 				});
+			} 
+			res.status(201).json({
+				status: 201,
+				message: 'Successfully created',
+				data: result.rows[0]
 			});
+		})
 	},
 	getAllPartiesList(req, res){
-		DataModel.findAll(parties, 'party')
-			.then(parties => res.status(200).json({
+		DataModel.execute(queryAll, [], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (result.rowCount < 1){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No party found'
+				});
+			}
+			res.status(200).json({
 				status: 200,
 				message: 'Success',
-				data: parties
-			}))
-			.catch(err => {
-				let codeStatus = err.status?err.status:500;
-				let response = {
-					status:codeStatus,
-					error:err.message
-				};
-				return res.status(codeStatus).json(response);
+				data: result.rows
 			});
+		})
 	},
 	getSpecificParty(req, res){
-		const errors = req.validationErrors();
-		if (errors) return res.status(400).json({status: 400, error: errors[0].msg});
 		const partyId = Number(req.params.partyId);
 
-		DataModel.findOneById(parties, 'party', partyId)
-			.then(party => res.status(200).json({
+		DataModel.execute(queryOne, [partyId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No party found'
+				});
+			}
+			res.status(200).json({
 				status: 200,
 				message: 'Success',
-				data: party
-			}))
-			.catch(err => {
-				let codeStatus = err.status?err.status:500;
-				let response = {
-					status:codeStatus,
-					error:err.message
-				};
-				return res.status(codeStatus).json(response);
+				data: result.rows[0]
 			});
+		})
 	},
 	modifyParty(req, res){
-		req.body.dataId = req.params.partyId;
-		DataModel.findOneAndUpdate(partyFileJson, parties, 'party', req.body)
-			.then(party => res.status(200).json({
-				status: 200,
-				message: 'Successfully modified',
-				data: party
-			}))
-			.catch(err => {
-				let codeStatus = err.status?err.status:500;
-				let response = {
-					status:codeStatus,
-					error:err.message
-				};
-				res.status(codeStatus).json(response);
-			});
+		const values = [
+			req.body.name,
+			req.body.hqAddress,
+			req.body.logoUrl,
+			req.params.partyId
+		] 
+		DataModel.execute(queryOne, [req.params.partyId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No party found'
+				});
+			}
+			DataModel.execute(queryEdit, values, (error, response)=>{
+				console.log(response)
+				if(error){
+					return res.status(500).json({ 
+						status: 500,
+						error: 'Service not availavle'
+					});
+				}
+				res.status(200).json({
+					status: 200,
+					message: 'Successfully modified',
+					data: response.rows[0]
+				});
+			})
+		})
 	},
 	deleteParty(req, res){
 		const partyId = Number(req.params.partyId);
 
-		DataModel.removeOne(partyFileJson, parties, 'party', partyId)
-			.then(() => res.status(200).json({
-				status: 200,
-				message: 'The party has been deleted'
-			}))
-			.catch(err => {
-				let codeStatus = err.status?err.status:500;
-				let response = {
-					status:codeStatus,
-					error:err.message
-				};
-				res.status(codeStatus).json(response);
-			});
+		DataModel.execute(queryOne, [partyId], (err, result)=>{
+			if(err){
+				return res.status(500).json({ 
+					status: 500,
+					error: 'Service not availavle'
+				});
+			} 
+			if (!result.rows[0]){
+				return res.status(404).json({ 
+					status: 404,
+					error: 'No office found'
+				});
+			}
+			DataModel.execute(queryDelete, [partyId], (error, response)=>{
+				console.log(response)
+				if(error){
+					return res.status(500).json({ 
+						status: 500,
+						error: 'Service not availavle'
+					});
+				}
+				res.status(200).json({
+					status: 200,
+					message: 'The office has been deleted'
+				});
+			})
+		})
 	}
 };
 
